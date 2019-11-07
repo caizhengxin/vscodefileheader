dd448b1bb5eb5f008cb18005870e10e8c2dfc030
JanKinCai <caizhengxin16@163.com>
2019-11-07 13:03:09 +0800

    refactor(demo): add C、C++ demo

---
+++ demo/test.c
@@ -0,0 +1,15 @@
+/*
+* @Author: JanKinCai
+* @Date:   2019-11-07 13:01:37
+* @Last Modified by:   JanKinCai
+* @Last Modified time: 2019-11-07 13:01:37
+*/
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
---
+++ demo/test.cpp
@@ -0,0 +1,17 @@
+/*
+* @Author: JanKinCai
+* @Date:   2019-11-07 13:01:59
+* @Last Modified by:   JanKinCai
+* @Last Modified time: 2019-11-07 13:01:59
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
