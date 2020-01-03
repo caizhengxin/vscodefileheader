5d19c36ecceedf230aa00e43a32543c267a208d5
JanKinCai <caizhengxin16@163.com>
2020-01-03 22:10:24 +0800

    style(extension.ts): update comment format

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,10 @@
+/**
+ * @Author: JanKinCai
+ * @Date:   2020-01-03 22:02:02
+ * @Last Modified by:   JanKinCai
+ * @Last Modified time: 2020-01-03 22:02:21
+ */
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -66,24 +73,24 @@ const file_suffix_mapping:any = {
 };
 
 
-/*
+/**
  * getConfig
  *
- * @return: any
+ * @return any
  */
-function getConfig():any{
+function getConfig(): any {
 	return vscode.workspace.getConfiguration("fileheader");
 }
 
 
-/*
+/**
  * getPathObject
  *
- * @editor(any): editText object.
+ * @param editor(any): editText object.
  * 
- * @return: any
+ * @return any
  */
-function getPathObject(editor: any): any{
+function getPathObject(editor: any): any {
 	/*
 	 * root
 	 * dir
@@ -95,83 +102,83 @@ function getPathObject(editor: any): any{
 }
 
 
-/*
+/**
  * isSuffix
  *
- * @editor(any): editText object.
- * @suffix(string): File suffix name, eg: `.php`,
+ * @param editor(any): editText object.
+ * @param suffix(string): File suffix name, eg: `.php`,
  * 
- * @return: boolean
+ * @return boolean
  */
-function isSuffix(editor: any, suffix: string): boolean{
+function isSuffix(editor: any, suffix: string): boolean {
 	return getPathObject(editor).ext.lastIndexOf(suffix) !== -1;
 }
 
 
-/*
+/**
  * isSuffixList
  *
- * @editor(any): editText object.
- * @suffixs(any): File suffix name list, eg: [`.php`],
+ * @param editor(any): editText object.
+ * @param suffixs(any): File suffix name list, eg: [`.php`],
  * 
- * @return: boolean
+ * @return boolean
  */
-function isSuffixList(editor: any, suffixs: any): boolean{
+function isSuffixList(editor: any, suffixs: any): boolean {
 	return suffixs.indexOf(getPathObject(editor).ext) !== -1;
 }
 
 
-/*
+/**
  * getActivePath
  *
- * @editor(any): editText object.
+ * @param editor(any): editText object.
  * 
- * @return: string
+ * @return string
  */
-function getActivePath(editor:any):string{
+function getActivePath(editor: any): string {
 	return getPathObject(editor).dir;
 }
 
 
-/*
+/**
  * getSuffix
  *
- * @editor(any): editText object.
+ * @param editor(any): editText object.
  * 
- * @return: string
+ * @return string
  */
-function getSuffix(editor: any):string{
+function getSuffix(editor: any): string {
 	let pathobj:any = path.parse(editor.document.fileName);
 
 	return pathobj.ext.toLowerCase() || pathobj.name.toLowerCase();
 }
 
 
-/*
+/**
  * getFileName
  *
- * @editor(any): editText object.
+ * @param editor(any): editText object.
  * 
- * @return: string
+ * @return string
  */
-function getFileName(editor:any):string{
+function getFileName(editor: any): string {
 	return path.parse(editor.document.fileName).name.toLowerCase();
 }
 
 
-/*
+/**
  * Match line
  *
- * @editor(any): editText object.
- * @value(string): Match value.
- * @max_line(number): Match max line.
+ * @param editor(any): editText object.
+ * @param value(string): Match value.
+ * @param max_line(number): Match max line.
  * 
- * @return: number, Not match line return -1
+ * @return number: Not match line return -1
  */
-function matchLine(editor:any, value:string, max_line:number=header_max_line):number{
-	let document = editor.document;
-	let lineCount:number = document.lineCount;
-	let i:number = 0;
+function matchLine(editor: any, value: string, max_line: number=header_max_line): number {
+	let document: any = editor.document;
+	let lineCount: number = document.lineCount;
+	let i: number = 0;
 
 	if(lineCount > max_line){
 		lineCount = max_line;
@@ -187,34 +194,34 @@ function matchLine(editor:any, value:string, max_line:number=header_max_line):nu
 }
 
 
-/*
+/**
  * Delete edittext comment
  *
- * @editor(any): editText object.
- * @value(string): Match value.
- * @max_line(number): Match max line.
+ * @param editor(any): editText object.
+ * @param value(string): Match value.
+ * @param max_line(number): Match max line.
  * 
- * @return: void
+ * @return void
  */
-function deleteEditorComment(editor:any, value:string, max_line:number=header_max_line):void{
-	let line:number = matchLine(editor, value, max_line);
+function deleteEditorComment(editor: any, value: string, max_line: number=header_max_line): void {
+	let line: number = matchLine(editor, value, max_line);
 
 	if(line !== -1){
-		editor.edit(function(editobj:any){
+		editor.edit((editobj: any) => {
 			editobj.delete(new vscode.Range(line, 0, line, value.length));
 		});
 	}
 }
 
 
-/*
+/**
  * deleteEditorComments
  *
- * @editor(any): editText object.
+ * @param editor(any): editText object.
  * 
- * @return: void
+ * @return void
  */
-function deleteEditorComments(editor:any):void{
+function deleteEditorComments(editor: any): void {
 	if(isSuffix(editor, ".php")){
 		deleteEditorComment(editor, "<?php", 2);
 	}else if(isSuffixList(editor, [".py", ".pxd", ".pyx"])){
@@ -223,18 +230,16 @@ function deleteEditorComments(editor:any):void{
 }
 
 
-/*
+/**
  * deleteEditorComments
  *
- * @editor(any): editText object.
- * @ignore(string[]): Filter rules.
+ * @param editor(any): editText object.
+ * @param ignore(string[]): Filter rules.
  * 
- * @return: boolean
+ * @return boolean
  */
-function isIgnore(editor:any, ignore: string[]):boolean{
-	let pathobj:any = getPathObject(editor);
-
-	console.log("sss".match("[a-z]+"));
+function isIgnore(editor: any, ignore: string[]): boolean {
+	let pathobj: any = getPathObject(editor);
 
 	for(let ige of ignore){
 		let reg: any = new RegExp(ige.replace("*", ".*"));
@@ -249,80 +254,92 @@ function isIgnore(editor:any, ignore: string[]):boolean{
 }
 
 
-// Get Time
-function get_datetime(fmt:any="YYYY-MM-DD HH:mm:ss"):string{
+/**
+ * getDateTime
+ *
+ * @param fmt(string): DateTime format, default ``YYYY-MM-DD HH:mm:ss``.
+ * 
+ * @return string
+ */
+function getDateTime(fmt: string="YYYY-MM-DD HH:mm:ss"): string {
 	return moment().format(fmt);
 }
 
 
-// Get default template
-function get_default_template():string{
-	return  path.join(path.dirname(__dirname), "template");
+/**
+ * getDefaultTemplate
+ * 
+ * @return string
+ */
+function getDefaultTemplate(): string {
+	return path.join(path.dirname(__dirname), "template");
 }
 
 
-// Get line
-function get_line(editor:any, str:string, line:number=header_max_line):number{
-	let document = editor.document;
-	let lineCount:number = document.lineCount;
-	let i:number = 0;
-
-	if(lineCount > 10){
-		lineCount = line;
-	}
-
-	for(i = 0; i <= lineCount - 1; i++){
-		if (document.lineAt(i).text.indexOf(str) !== -1){
-			return i;
-		}
-	}
-
-	return -1;
-}
-
-
-// Judge if the head exists
-function is_header(editor:any):boolean{
-	let text:string = editor.document.getText(0, 0, header_max_line + 1, 0);
-
-	if(text.indexOf("@Author:") !== -1 && text.indexOf("@Last Modified by:") !== -1){
+/**
+ * Judge if the head exists
+ * 
+ * @param editor(any): editText object.
+ * 
+ * @return boolean: Judge if the head exists.
+ */
+function isHeaderExists(editor: any): boolean {
+	if(matchLine(editor, "Author:") !== -1 && matchLine(editor, "Last Modified by:") !== -1){
 		return true;
 	}
+
 	return false;
 }
 
 
-// Get Template
-function get_tmpl(editor:any, config:any, tmplpath:string="", type:string="header"):string{
-	let suffix:string = getSuffix(editor);
-	let name:string = getFileName(editor);
-
-	let tmpl:string = (config.file_suffix_mapping[name + suffix] || config.file_suffix_mapping[suffix] || file_suffix_mapping[suffix]) + ".tmpl";
-	// var tmpl_path:string = config.custom_template_path || get_default_template();
+/**
+ * Get template
+ * 
+ * @param editor(any): editText object.
+ * @param config(any): VScode config.
+ * @param tmplpath(string): Template path, default ``""``.
+ * @param type(string): body or header, default ``header``
+ * 
+ * @return boolean: Template path.
+ */
+function getTemplatePath(editor: any, config: any, tmplpath: string="", type: string="header"): string {
+	let suffix: string = getSuffix(editor);
+	let name: string = getFileName(editor);
+	let tmpl: string = (config.file_suffix_mapping[name + suffix] || config.file_suffix_mapping[suffix] || file_suffix_mapping[suffix]) + ".tmpl";
 
 	return path.join(tmplpath || config.custom_template_path , type, tmpl);
 }
 
 
-// Open Template
-function open_tmpl(editor:any, config:any, type:string="header", callback:any){
-	let tmpl_path:string = get_tmpl(editor, config, "", type);
+/**
+ * openTemplate
+ * 
+ * @param editor(any): editText object.
+ * @param config(any): VScode config.
+ * @param type(string): body or header, default ``header``
+ * @param callback(any): Callback.
+ * 
+ * @return void
+ */
+function openTemplate(editor: any, config: any, type: string="header", callback: any): void {
+	let tmpl_path: string = getTemplatePath(editor, config, "", type);
 
 	fs.exists(tmpl_path, (exists) => {
 		if(exists){
+			// Custom template
 			vscode.workspace.fs.readFile(vscode.Uri.file(tmpl_path)).then(s => {
 				callback(s);
-			});	
+			});
 		}else{
-			// 默认模板
-			tmpl_path = get_tmpl(editor, config, get_default_template(), type);
+			// Default template
+			tmpl_path = getTemplatePath(editor, config, getDefaultTemplate(), type);
 			fs.exists(tmpl_path, (exists) => {
 				if(exists){
 					vscode.workspace.fs.readFile(vscode.Uri.file(tmpl_path)).then(s => {
 						callback(s);
 					});	
 				}else{
-					callback("");
+					// callback("");
 					console.log("Not found fileheader template: " + tmpl_path);
 				}
 			});		
@@ -331,36 +348,46 @@ function open_tmpl(editor:any, config:any, type:string="header", callback:any){
 }
 
 
-// Update Header
-function update_header(editor:any, config:any):void{
-	editor.edit(function(editobj:any){
+/**
+ * updateHeader
+ * 
+ * @param editor(any): editText object.
+ * @param config(any): VScode config.
+ * 
+ * @return void
+ */
+function updateHeader(editor: any, config: any): void {
+	editor.edit((editobj: any) => {
+		let line: number = matchLine(editor, "Last Modified time:", 8);
+		let start: number = editor.document.lineAt(line).text.indexOf(":") + 1;
+		editobj.replace(new vscode.Range(line, start, line, 100), " " + getDateTime());
 
-		let date:string = get_datetime();
-		let line:number = get_line(editor, "@Last Modified time:");
-		let start:number = editor.document.lineAt(line).text.indexOf(":") + 1;
-
-		editobj.replace(new vscode.Range(line, start, line, 100), " " + date);
-
-		line = get_line(editor, "@Last Modified by:");
+		line = matchLine(editor, "@Last Modified by:", 8);
 		start = editor.document.lineAt(line).text.indexOf(":") + 1;
-	
 		editobj.replace(new vscode.Range(line, start, line, 100), "   " + config.author);
 	});
 
-	editor.document.save();	
+	editor.document.save();
 }
 
 
-// Insert Header or Body
-function insert_header_body(editor:any, config:any):void{
-	let lineCount:number = editor.document.lineCount;
+/**
+ * insertHeaderBody
+ * 
+ * @param editor(any): editText object.
+ * @param config(any): VScode config.
+ * 
+ * @return void
+ */
+function insertHeaderBody(editor: any, config: any): void {
+	let lineCount: number = editor.document.lineCount;
 
+	// Delete comment
 	deleteEditorComments(editor);
 
-	open_tmpl(editor, config, "header", function(s:any){
-		let date:string = get_datetime();
-
-		let ret:any = template.render(s.toString(), Object.assign(
+	openTemplate(editor, config, "header", (s:any) => {
+		let date: string = getDateTime();
+		let ret: any = template.render(s.toString(), Object.assign(
 			{
 				author: config.author,
 				create_time: date,
@@ -371,19 +398,20 @@ function insert_header_body(editor:any, config:any):void{
 		));
 
 		if(lineCount <= 1){
-			open_tmpl(editor, config, "body", function(s:any){
+			openTemplate(editor, config, "body", (s:any) => {
 				ret += s.toString() + "\r\n";
 
-				editor.edit(function(editobj:any){
+				editor.edit((editobj:any) => {
 					editobj.insert(new vscode.Position(0, 0), ret);
 				});
+
 				editor.document.save();
 			});
 		}else{
-			console.log(ret);
-			editor.edit(function(editobj:any){
+			editor.edit((editobj:any) => {
 				editobj.insert(new vscode.Position(0, 0), ret);
 			});
+
 			editor.document.save();					
 		}
 	});	
@@ -403,12 +431,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.fileheader', () => {
 		// The code you place here will be executed every time your command is executed
-		let config:any = getConfig();
-		let editor:any = vscode.window.activeTextEditor;
+		let config: any = getConfig();
+		let editor: any = vscode.window.activeTextEditor;
 
-
-		if(!is_header(editor)){
-			insert_header_body(editor, config);
+		if(!isHeaderExists(editor)){
+			insertHeaderBody(editor, config);
 		}
 	});
 
@@ -416,27 +443,26 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	// Save
-	vscode.workspace.onWillSaveTextDocument(e => {
-		let config:any = getConfig();
-		let editor:any = vscode.window.activeTextEditor;
-
+	vscode.workspace.onWillSaveTextDocument(() =>{
+		let config: any = getConfig();
+		let editor: any = vscode.window.activeTextEditor;
 
 		// Update Header
-		if(is_header(editor)){
-			update_header(editor, config);
+		if(isHeaderExists(editor)){
+			updateHeader(editor, config);
 		}else if(config.save && isIgnore(editor, config.ignore)){
-			insert_header_body(editor, config);
+			insertHeaderBody(editor, config);
 		}
 	});
 
 
 	// Open
-	vscode.workspace.onDidOpenTextDocument(e => {
-		let config:any = getConfig();
-		let editor:any = vscode.window.activeTextEditor;
+	vscode.workspace.onDidOpenTextDocument(() => {
+		let config: any = getConfig();
+		let editor: any = vscode.window.activeTextEditor;
 
-		if(config.open && !is_header(editor) && isIgnore(editor, config.ignore)){
-			insert_header_body(editor, config);
+		if(config.open && !isHeaderExists(editor) && isIgnore(editor, config.ignore)){
+			insertHeaderBody(editor, config);
 		}
 	});
 }
